85e247dbe109602f984486d5adf2bd1935e3f539
Rachit Saxena <72144798+horcruxxxx@users.noreply.github.com>
2023-01-26 23:17:54 +0530

    Frontend Completed
    Both signup and login page completed using useState() hook. Starting Backend....

--- front-end/src/components/login/login.js
+++ front-end/src/components/login/login.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import "./login.css";
 
 const Login =()=>{
+
+    const [ user, setUser] = useState({         //creating hook for maintaing all input elements.
+        email:"",
+        password:""
+    });
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }  
+
     return(
         <div className="login">
             <h1>Login-Page</h1>
-            <input type="email" Placeholder="Enter your Email"></input>
-            <input type="password" placeholder="Enter Password"></input>
+            <input type="email" name="email" value={user.email} onChange={handleChange} Placeholder="Enter your Email"></input>
+            <input type="password" name="password" value={user.password} onChange={handleChange} placeholder="Enter Password"></input>
             <div className="button">Login</div>
             <div> or </div>
             <div className="button">Register</div>
--- front-end/src/components/signup/signup.js
+++ front-end/src/components/signup/signup.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React,{useState} from "react";
 import "./signup.css";
 
 const Signup =()=>{
+    
+    const [ user, setUser] = useState({         //creating hook for maintaing all input elements.
+        fname: "",
+        lname:"",
+        email:"",
+        password:""
+    });
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }    
+
+    // const handleChange = event => {
+    //     const { name, value } = event.target
+    //     setUser({
+    //         ...user,
+    //         [name]: value
+    //     })
+    // }
+
     return(
         <div className="signup">
             <h1>SignUp-Page</h1>
-            <input type="text" placeholder="Enter First Name"></input>
-            <input type="text" placeholder="Enter Last Name"></input>
-            <input type="email" Placeholder="Enter your Email"></input>
-            <input type="password" placeholder="Enter Password"></input>
+            <input type="text" name="fname" value={user.fname} onChange={handleChange} placeholder="Enter First Name"></input>
+            <input type="text" name="lname" value={user.lname} onChange={handleChange} placeholder="Enter Last Name"></input>
+            <input type="email" name="email" value={user.email} onChange={handleChange} placeholder="Enter your Email"></input>
+            <input type="password" name="password" value={user.password} onChange={handleChange} placeholder="Enter Password"></input>
             
             <div className="button">Register</div>
         </div>
