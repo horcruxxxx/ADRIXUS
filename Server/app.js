428bb5724b998dd505c0d052c055a381e9b0e9a4
Rachit Saxena <72144798+horcruxxxx@users.noreply.github.com>
2023-01-26 17:57:06 +0530

    initial commit
    all setup done, all dependencies installed. Good to start.

---
+++ Server/app.js
@@ -0,0 +1,22 @@
+// const express = require("express");
+// const app = express();
+
+
+
+
+
+
+
+
+
+
+
+// app.get("/",function(req,res){
+//     res.send("Server is responding");
+// });
+
+
+
+// app.listen(3000,function(){
+//     console.log("Server Started");
+// });
