45bcc52172923904778dc5b0dc52c86a3acb3f55
Rachit Saxena <72144798+horcruxxxx@users.noreply.github.com>
2023-02-01 10:37:01 +0530

    modified homepage
    implemented filtering in the homepage table using mock-data and react-table module.
    
    remaining tasks:
    Pagination
    hashing passwords
    sorting

---
+++ front-end/src/components/columns.js
@@ -0,0 +1,18 @@
+export const COLUMNS = [
+    {
+      Header: 'Id',
+      accessor: 'id',
+    },
+    {
+      Header: 'First Name',
+      accessor: 'fname',
+    },
+    {
+        Header: 'Last Name',
+        accessor: 'lname',
+    },
+    {
+        Header: 'Email',
+        accessor: 'email',
+    }
+]
--- front-end/src/components/homepage/homepage.js
+++ front-end/src/components/homepage/homepage.js
@@ -1,13 +1,61 @@
-import React from "react";
 import "./homepage.css";
+import React, { useMemo } from 'react'
+import { useTable, useGlobalFilter } from 'react-table'
+import MOCK_DATA from '../MOCK_DATA.json'
+import { COLUMNS } from '../columns'
+import { GlobalFilter } from "../Globalfilter";
 
 const Homepage =({setLoginUser})=>{
-    return(
-        <div className="homepage">
+  const columns = useMemo(() => COLUMNS, [])
+  const data = useMemo(() => MOCK_DATA, [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable({
+    columns,
+    data
+  },useGlobalFilter)
+
+  const {globalFilter} = state;
+
+  return (
+    <>
+    <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter}/>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      {/* <div className="homepage">
             <h1>Hello Homepage</h1>
             <div className="button" onClick={() => setLoginUser({})} >Logout</div>
-        </div>
-    )
-};
+      </div> */}
+    </>
+  )
+}
 
 export default Homepage;
