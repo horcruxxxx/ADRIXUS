c25fa1b2dac2410cdb81783799e2d892bef50f03
Rachit Saxena <72144798+horcruxxxx@users.noreply.github.com>
2023-02-02 22:35:12 +0530

    Sorting implimented

--- front-end/src/components/homepage/homepage.js
+++ front-end/src/components/homepage/homepage.js
@@ -1,13 +1,57 @@
 import "./homepage.css";
-import React, { useMemo } from 'react'
+import React, { useMemo,useState } from 'react'
 import { useTable, useGlobalFilter,usePagination } from 'react-table'
 import MOCK_DATA from '../MOCK_DATA.json'
 import { COLUMNS } from '../columns'
 import { GlobalFilter } from "../Globalfilter";
 
 const Homepage =({setLoginUser})=>{
+
+  const [sortedData, setSortedData] = useState(MOCK_DATA);
+
+  const onSortingChange = (e) => {
+    // console.log(sortedArray);
+    const sortKey = e.target.value;
+    const sortedArray = [...MOCK_DATA];
+    console.log(sortedArray);
+    let temp = [];
+    // console.log(sortKey);
+    switch (sortKey) {
+      case "Fname":
+        console.log("1");
+        temp  = sortedArray.sort((a, b) => {
+          if (a.firstName < b.firstName) return -1;
+          if (a.firstName > b.firstName) return 1;
+          return 0;
+        });
+        break;
+      case "Lname":
+        console.log("2");
+        temp = sortedArray.sort((a, b) => {
+          if (a.lastName < b.lastName) return -1;
+          if (a.lastName > b.lastName) return 1;
+          return 0;
+        });
+        break;
+      case "Email":
+        console.log("3");
+        temp = sortedArray.sort((a, b) => {
+          if (a.email < b.email) return -1;
+          if (a.email > b.email) return 1;
+          return 0;
+        });
+        break;
+      default:
+        console.log("none");
+        temp = sortedArray;
+        break;
+    }
+    console.log(temp);
+    setSortedData(temp);
+  };
+
   const columns = useMemo(() => COLUMNS, [])
-  const data = useMemo(() => MOCK_DATA, [])
+  const data = useMemo(() => sortedData, [])
 
   const {
     getTableProps,
@@ -58,6 +102,15 @@ const Homepage =({setLoginUser})=>{
         <button onClick={()=> previousPage()} disabled={!canPreviousPage}>Previous</button>
         <button onClick={()=> nextPage()} disabled={!canNextPage}>Next</button>
         <button  onClick={() => setLoginUser({})} >Logout</button>
+      </div>
+      <div className="sort-section">
+      Sort by: 
+        <select onChange={(e)=>onSortingChange(e)}>
+          <option value=''>Select Option</option>
+          <option value='Fname'>First Name</option>
+          <option value='Lname'>Last Name</option>
+          <option value='Email'>Email</option>
+        </select>
       </div>
       <div className="footer">
             <h1>ADRIXUS DASHBOARD</h1>
